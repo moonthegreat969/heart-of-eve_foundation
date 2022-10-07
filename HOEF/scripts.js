6fc8d85a589317ddc9cf703af8a3affd80b699b6
OgunsanwoDavid <100451541+ogunsanwodavid@users.noreply.github.com>
2022-10-07 06:26:29 -0700

    added source files
    added html , css and js files. i also added images.

---
+++ HOEF/scripts.js
@@ -0,0 +1,159 @@
+//code of modal 1
+const modal1 = document.getElementById("modal1");
+const trigger1 = document.getElementById("trigger1");
+const closeButton1 = document.getElementById("closebutton1");
+
+function toggleModal1() {
+    modal1.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal1) {
+        toggleModal1();
+    }
+}
+
+trigger1.addEventListener("click", toggleModal1);
+closeButton1.addEventListener("click", toggleModal1);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 2
+const modal2 = document.getElementById("modal2");
+const trigger2 = document.getElementById("trigger2");
+const closeButton2 = document.getElementById("closebutton2");
+
+function toggleModal2() {
+    modal2.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal2) {
+        toggleModal2();
+    }
+}
+
+trigger2.addEventListener("click", toggleModal2);
+closeButton2.addEventListener("click", toggleModal2);
+window.addEventListener("click", windowOnClick);
+
+//Code of Modal 3
+const modal3 = document.getElementById("modal3");
+const trigger3 = document.getElementById("trigger3");
+const closeButton3 = document.getElementById("closebutton3");
+
+function toggleModal3() {
+    modal3.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal3) {
+        toggleModal3();
+    }
+}
+
+trigger3.addEventListener("click", toggleModal3);
+closeButton3.addEventListener("click", toggleModal3);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 4
+
+const modal4 = document.getElementById("modal4");
+const trigger4 = document.getElementById("trigger4");
+const closeButton4 = document.getElementById("closebutton4");
+
+function toggleModal4() {
+    modal4.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal4) {
+        toggleModal4();
+    }
+}
+
+trigger4.addEventListener("click", toggleModal4);
+closeButton4.addEventListener("click", toggleModal4);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 5
+
+const modal5 = document.getElementById("modal5");
+const trigger5 = document.getElementById("trigger5");
+const closeButton5 = document.getElementById("closebutton5");
+
+function toggleModal5() {
+    modal5.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal5) {
+        toggleModal5();
+    }
+}
+
+trigger5.addEventListener("click", toggleModal5);
+closeButton5.addEventListener("click", toggleModal5);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 6
+
+const modal6 = document.getElementById("modal6");
+const trigger6 = document.getElementById("trigger6");
+const closeButton6 = document.getElementById("closebutton6");
+
+function toggleModal6() {
+    modal6.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal6) {
+        toggleModal6();
+    }
+}
+
+trigger6.addEventListener("click", toggleModal6);
+closeButton6.addEventListener("click", toggleModal6);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 7
+
+const modal7 = document.getElementById("modal7");
+const trigger7 = document.getElementById("trigger7");
+const closeButton7 = document.getElementById("closebutton7");
+
+function toggleModal7() {
+    modal7.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal7) {
+        toggleModal7();
+    }
+}
+
+trigger7.addEventListener("click", toggleModal7);
+closeButton7.addEventListener("click", toggleModal7);
+window.addEventListener("click", windowOnClick);
+
+//code of modal 8
+
+const modal8 = document.getElementById("modal8");
+const trigger8 = document.getElementById("trigger8");
+const closeButton8 = document.getElementById("closebutton8");
+
+function toggleModal8() {
+    modal8.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal8) {
+        toggleModal8();
+    }
+}
+
+trigger8.addEventListener("click", toggleModal8);
+closeButton8.addEventListener("click", toggleModal8);
+window.addEventListener("click", windowOnClick);
+
+
+
